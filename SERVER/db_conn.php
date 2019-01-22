ff3c01f9232fb4d9a3ea89bb1ce485c6341fb32a
filmar <filippo.ufficiale@gmail.com>
2019-01-22 14:10:09 +0100

    commit necessario

--- SERVER/db_conn.php
+++ SERVER/db_conn.php
@@ -3,6 +3,6 @@
 define('HOST', 'localhost');
 define('USER', 'root');
 define('PASS', '');
-define('DB', 'my_taclife');
+define('BD','my_taclife');
 //connessione al database
 $con=mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
